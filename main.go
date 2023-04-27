5c57f2756fa7e04655b10ee40733ce7d4f112f91
Queetin <1589893175@qq.com>
2023-04-27 13:09:24 +0800

    try

--- main.go
+++ main.go
@@ -14,13 +14,42 @@ import (
 	//"time"
 )
 
+var wg sync.WaitGroup
+var client *http.Client
+func init() {
+	     tr := &http.Transport{
+	     	DisableKeepAlives:     false,
+	     	DisableCompression:    false,
+	     	MaxIdleConns:          0,
+	     	MaxIdleConnsPerHost:   1024,
+	     	MaxConnsPerHost:       0,
+	     	IdleConnTimeout:       0,
+	     	ResponseHeaderTimeout: 0,
+	     	ExpectContinueTimeout: 0,
+	     	MaxResponseHeaderBytes: 0,
+	     	WriteBufferSize:        0,
+	     	ReadBufferSize:         0,
+	     	ForceAttemptHTTP2:      false,
+	     }
+		 client = &http.Client{Transport: tr}
+		}
 func main() {
 	username := flag.String("u", "admin", "input your username")
 	password := flag.String("p", "admin", "input your password")
-	repositoryurl := flag.String("r", "http://nexus.z-bank.com", "input your repository url")
+	repositoryurl := flag.String("r", "http://nexus.com", "input your repository url")
 	flag.Parse()
-	var wg sync.WaitGroup
+    const maxConcurrent = 600
+	filech := make(chan string,1)
+	wg.Add(1)
+	go uploadfile(*username, *password, *repositoryurl, filech,maxConcurrent)
+	wg.Add(1)
+	go findfile(filech)
 	wg.Wait()
+
+}
+
+func findfile(ch chan string) {
+	defer wg.Done()
 	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			fmt.Println(err)
@@ -33,55 +62,66 @@ func main() {
 			return err
 		}
 		if !info.IsDir() && !matched {
-			wg.Add(1)
-			go func(file string) {
-				form := new(bytes.Buffer)
-				writer := multipart.NewWriter(form)
-				fw, err := writer.CreateFormFile("fileUploadName", path)
-				if err != nil {
-					fmt.Println(err)
-				}
-				fd, err := os.Open(path)
-				if err != nil {
-					fmt.Println(err)
-				}
-				defer fd.Close()
-				_, err = io.Copy(fw, fd)
-				if err != nil {
-					fmt.Println(err)
-				}
-
-				writer.Close()
-				tr := &http.Transport{
-					MaxIdleConns:          10,
-					//IdleConnTimeout:       60 * time.Second,
-					IdleConnTimeout:  0,
-					//ResponseHeaderTimeout: 60 * time.Second,
-					ResponseHeaderTimeout: 0,
-					DisableKeepAlives:     false,
-				}
-				client := &http.Client{Transport: tr}
-				url := *repositoryurl + path
-				req, err := http.NewRequest("PUT", url, form)
-				if err != nil {
-					fmt.Println(err)
-				}
-				req.Header.Set("Content-Type", writer.FormDataContentType())
-				req.SetBasicAuth(*username, *password)
-				resp, err := client.Do(req)
-				if err != nil {
-					fmt.Println(err)
-				}
-				wg.Done()
-				if _, err := io.Copy(io.Discard, resp.Body); err != nil {
-					fmt.Println(err)
-				}
-				resp.Body.Close()
-			}(path)
+			ch <- path
 		}
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
+	close(ch)
+}
+
+func uploadfile(username, password, repositoryurl string, ch chan string,maxConcurrent int) {
+	defer wg.Done()
+	var innerwg sync.WaitGroup
+	sem := make(chan struct{}, maxConcurrent)
+	for path := range ch {
+		sem <- struct{}{}
+		innerwg.Add(1)
+		go func(f string) {
+			defer innerwg.Done()
+			defer func() { <-sem }()
+			form := new(bytes.Buffer)
+			writer := multipart.NewWriter(form)
+			fw, err := writer.CreateFormFile("fileUploadName", f)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fd, err := os.Open(f)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			_, err = io.Copy(fw, fd)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			writer.Close()
+			/*
+			tr := &http.Transport{
+				MaxIdleConns: 0,
+				IdleConnTimeout:       180 * time.Second,
+				ResponseHeaderTimeout: 60 * time.Second,
+				DisableKeepAlives:     false,
+			}
+			client := &http.Client{Transport: tr}
+			*/
+			url := repositoryurl + f
+			req, err := http.NewRequest("PUT", url, form)
+			if err != nil {
+				fmt.Println(err)
+			}
+			req.Header.Set("Content-Type", writer.FormDataContentType())
+			req.SetBasicAuth(username, password)
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println(err)
+			}
+            io.Copy(io.Discard, resp.Body);
+			defer resp.Body.Close()
+		}(path)
+	}
+	innerwg.Wait()
 }
